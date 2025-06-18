f261353872e48ca6f04ed6e1ac2c34ec2330e5e6
Pankaj Manral <pankaj_manral@ildc-inlp.idealake.com>
2025-06-18 16:31:40 +0530

    added dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,3 @@
-// const swiper = new Swiper('.swiper', {
-//     loop: false,
-//     spaceBetween: 24,
-//     breakpoints: {
-//         1200: {
-//             slidesPerView: 4
-//         },
-//         1024: {
-//             slidesPerView: 2.5,
-//         }
-//     }
-// })
-
 let swiperInstance;
 function handleSwiper() {
     if (window.innerWidth <= 768) {
@@ -44,3 +31,13 @@ function handleSwiper() {
 
 window.addEventListener('resize', handleSwiper);
 window.addEventListener('DOMContentLoaded', handleSwiper);
+
+const toggleBtn = document.getElementById('toggle-btn')
+toggleBtn.addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('dark')
+    if(document.querySelector('body').classList.contains('dark')){
+        document.querySelector('body').style.backgroundColor = '#171717'
+    }else{
+        document.querySelector('body').style.backgroundColor = '#FFF'
+    }
+})
